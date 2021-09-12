923818428028c777e6da5fabd1947fa5a52338af
Andrey Talanin <andrey.talanin@outlook.com>
2021-09-12 15:37:14 +0300

    Add the IArtistDirectoryProcessor service interface and a placeholder implementation for it

---
+++ Source/MusicLibraryCatalogBuilder/Services/ArtistDirectoryProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+using MusicLibraryCatalogBuilder.Entities;
+using MusicLibraryCatalogBuilder.Services.Interfaces;
+
+namespace MusicLibraryCatalogBuilder.Services
+{
+    public class ArtistDirectoryProcessor : IArtistDirectoryProcessor
+    {
+        public bool CheckIfArtistDirectory(DirectoryInfo directoryInfo) => throw new NotImplementedException();
+
+        public ArtistDirectoryInfo ParseArtistDirectoryInfo(DirectoryInfo directoryInfo) => throw new NotImplementedException();
+    }
+}
---
+++ Source/MusicLibraryCatalogBuilder/Services/Interfaces/IArtistDirectoryProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+using MusicLibraryCatalogBuilder.Entities;
+
+namespace MusicLibraryCatalogBuilder.Services.Interfaces
+{
+    public interface IArtistDirectoryProcessor
+    {
+        public bool CheckIfArtistDirectory(DirectoryInfo directoryInfo);
+
+        public ArtistDirectoryInfo ParseArtistDirectoryInfo(DirectoryInfo directoryInfo);
+    }
+}
