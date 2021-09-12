25b900b92799a14ba5b3e90b33ed98cc3e8b6ce5
Andrey Talanin <andrey.talanin@outlook.com>
2021-09-12 14:36:52 +0300

    Configure the application for the .NET Core hosting pattern

--- Source/MusicLibraryCatalogBuilder/Program.cs
+++ Source/MusicLibraryCatalogBuilder/Program.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
 
 namespace MusicLibraryCatalogBuilder
 {
     internal class Program
     {
-        private static void Main(string[] args) { }
+        private static async Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args);
+        }
     }
 }
