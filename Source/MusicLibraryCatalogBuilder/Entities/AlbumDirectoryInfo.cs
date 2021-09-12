4f9309837cdaf422228042d309f88e5b79d63d57
Andrey Talanin <andrey.talanin@outlook.com>
2021-09-12 15:27:28 +0300

    Add the ArtistDirectoryInfo and AlbumDirectoryInfo data entities

---
+++ Source/MusicLibraryCatalogBuilder/Entities/AlbumDirectoryInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace MusicLibraryCatalogBuilder.Entities
+{
+    public class AlbumDirectoryInfo
+    {
+        public int Number { get; set; }
+
+        public string AlbumName { get; set; } = string.Empty;
+
+        public int Year { get; set; }
+
+        public string Type { get; set; } = string.Empty;
+
+        public string Encoder { get; set; } = string.Empty;
+
+        public string Format { get; set; } = string.Empty;
+
+        public string Source { get; set; } = string.Empty;
+    }
+}
---
+++ Source/MusicLibraryCatalogBuilder/Entities/ArtistDirectoryInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MusicLibraryCatalogBuilder.Entities
+{
+    public class ArtistDirectoryInfo
+    {
+        public string ArtistName { get; set; } = string.Empty;
+
+        public int DiscographyFrom { get; set; }
+
+        public int DiscographyTo { get; set; }
+
+        public IList<string> Genres { get; set; } = new List<string>();
+
+        public IList<AlbumDirectoryInfo> Albums { get; set; } = new List<AlbumDirectoryInfo>();
+    }
+}
